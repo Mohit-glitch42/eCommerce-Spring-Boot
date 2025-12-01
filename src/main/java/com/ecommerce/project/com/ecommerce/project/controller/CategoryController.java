4e73a5576f966dde66f2178849aba51f229ee2d8
Mohit-glitch42 <2k22cscys2212018@gmail.com>
2025-12-01 11:57:07 +0530

    Added a category on the server using POST mapping

--- src/main/java/com/ecommerce/project/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/com/ecommerce/project/controller/CategoryController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.com.ecommerce.project.controller;
 
 import com.ecommerce.project.com.ecommerce.project.model.Category;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,17 @@ public class CategoryController {
 
     private List<Category> categories=new ArrayList<>();
 
+    //teh get mapping fetches the categories from post method and displays them
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
         return categories;
+    }
+
+    //we have been able to add new categories in our api with the help of post mapping
+    //we made use of post to help us create new resources on the server
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category){
+        categories.add(category);
+        return "Category added successfully";
     }
 }
