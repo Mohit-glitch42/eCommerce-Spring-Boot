426cc9af61e2dc6b2d16d124685acfb1751d6839
Mohit-glitch42 <2k22cscys2212018@gmail.com>
2025-12-01 18:57:29 +0530

    Added Update Category Functionality endpoint

--- src/main/java/com/ecommerce/project/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/com/ecommerce/project/service/CategoryService.java
@@ -9,4 +9,8 @@ public interface CategoryService {
     void createCategory(Category category);
 
     String deleteCategory(Long categoryId);
+
+    Category updateCategory(Category category);
+
+    Category updateCategory(Category category, Long categoryId);
 }
--- src/main/java/com/ecommerce/project/com/ecommerce/project/service/CategoryServiceImp.java
+++ src/main/java/com/ecommerce/project/com/ecommerce/project/service/CategoryServiceImp.java
@@ -7,13 +7,14 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class CategoryServiceImp implements CategoryService{
+public class CategoryServiceImp implements CategoryService {
 
-    private List<Category> categories=new ArrayList<>();
+    private List<Category> categories = new ArrayList<>();
     //kepping track of categories
-    private Long nextId=1L;
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -23,17 +24,39 @@ public class CategoryServiceImp implements CategoryService{
     @Override
     public void createCategory(Category category) {
         category.setCategoryId(nextId++);
-    categories.add(category);
+        categories.add(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
 
         //to remove the category we need to make use of category object because the user only passes in categoryId
-        Category category=categories.stream().filter(c->c.getCategoryId().equals(categoryId)).findFirst().
-                orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found"));
+        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found"));
 
         categories.remove(category);
-        return "Category with categoryId: " + categoryId + " deleted successfully" ;
+        return "Category with categoryId: " + categoryId + " deleted successfully";
+    }
+
+    @Override
+    public Category updateCategory(Category category) {
+        return null;
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+        Optional<Category> optionalCategory = categories.stream().
+                filter(c -> c.getCategoryId().equals(categoryId)).
+                findFirst();
+
+        if (optionalCategory.isPresent()) {
+            Category existingCategory = optionalCategory.get();
+            existingCategory.setCategoryName(category.getCategoryName());
+            return existingCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource Not Found");
+        }
     }
 }
+
+
